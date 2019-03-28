307347449aec52865da8be4a543a82d9515883aa
qwert2603 <qwert2603@mail.ru>
2019-03-28 20:38:32 +0700

    \

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_codegenny/sdg.dart';
-import 'package:sqflite/sqflite.dart';
 
-void main() async {
-  Database database = await openDatabase("test.db", version: 1,
-      onCreate: (Database db, int version) async {
-    await db.execute(
-        'CREATE TABLE Person (id INTEGER PRIMARY KEY, name TEXT, age REAL, isMale INT)');
-  });
-
-  PersonDaoImpl dao = PersonDaoImpl(database);
-  await dao.insertPersons([
-    Person.d(1, "alex", 24, true),
-    Person.d(2, "sss", 22, false),
-    Person.d(3, "qqq", 53, true),
-  ]);
-  print(await dao.getPersons([1]));
-  print(await dao.getPersons([2]));
-  print(await dao.getPersons([3]));
-  print(await dao.getPersons([1, 2, 3]));
-  print(await dao.deletePersons([3]));
-  print(await dao.getPersons([3]));
-  print(await dao.getPersons([1, 2, 3]));
-
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
